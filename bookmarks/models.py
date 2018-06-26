f739e2910d41f4c93bef4c80bf5fef8a15adb3fc
astro11 <aquila.strother@asu.edu>
2018-06-26 11:12:46 -0400

    added mehod to bookmarks model that defines a return instance

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -6,3 +6,6 @@ class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)    
     title = models.CharField(max_length=200)    
     created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return '%s %s' % (self.title, self.id)
