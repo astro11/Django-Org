5d0c9d21292d04561b166a7e0954676dc60801b5
astro11 <aquila.strother@asu.edu>
2018-06-26 16:43:20 -0400

    added and registered PersonalNote model. Also updated migrations

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -7,6 +7,7 @@ class Bookmark(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField(default='Your Website')    
     created_at = models.DateTimeField(auto_now_add=True)
-
+    category = models.CharField(max_length=20)
+    
 def __str__(self):
     return '%s %s' % (self.title, self.id)
--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from uuid import uuid4
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    #TODO: Add user/author - creator of note
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    #TODO: Tagging system or categories or other additions
+    category = models.CharField(max_length=20)
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
