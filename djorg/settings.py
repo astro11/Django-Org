4ad481f27578ecd9de53ba00e1d33db66fe23e0d
astro11 <aquila.strother@asu.edu>
2018-06-29 13:34:02 -0400

    proc edit

--- djorg/settings.py
+++ djorg/settings.py
@@ -9,13 +9,20 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 import os
-import dj_database_url
+
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -25,8 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
